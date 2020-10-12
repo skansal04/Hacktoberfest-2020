7b1ad993c72e1540d936a6ef9958bfe99eadd9ef
skansal04 <70471129+skansal04@users.noreply.github.com>
2020-10-12 18:59:04 +0530

    repo.py

--- Mini-Projects/Python/factorial.py
+++ Mini-Projects/Python/factorial.py
@@ -3,14 +3,14 @@ num = int(input('Enter the number: '))
 factorial = 1
 
 if num<0:
-  print("Factorial doesn't exist")  
+  print("Factorial doesn't exist for the number you entered")  
 
 elif num==0:
-  print("Factorial is 1")
+  print("Factorial is 1 for the number",num)
 
 else:
   for i in range(1,num+1):
     factorial = factorial*i
     
 
-  print("Factorial is",factorial)    
+  print("Factorial is",factorial,"for the number",num)    
